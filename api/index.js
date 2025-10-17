748d7cc1101004a9ac76889d775fa59c720998e1
Areesh <154906888+muhammadareesh@users.noreply.github.com>
2025-10-17 15:57:28 +0500

    again check changes

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! its me...");
+  res.send("Hello World! its me... Netro...");
 });
 
 app.listen(port, () => {
